886fe78fb93353696326e575e810af34809dd19f
Moob33 <2622856298@qq.com>
2025-05-10 19:56:45 +0800

    2

---
+++ DZY/Assets/jiaoben/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float qhsudu, zysudu;
+    public GameObject bullet;
+    private float verticalInput, horizontalInput;
+    public float jumpForce;//定义一个跳跃的力
+    public float zlbs;//定义重力倍数
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= zlbs;
+        rb = GetComponent<Rigidbody>();//获取挂载在玩家身上的刚体组件
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //verticalInput = Input.GetAxis("Vertical");//获取垂直轴输入数据
+        horizontalInput = Input.GetAxis("Horizontal");
+        //transform.Translate(Vector3.forward * Time.deltaTime * qhsudu * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * zysudu * horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.F))//获取键盘输入的值，按下键盘某个键
+        {
+            //生成对象（预制体，位置，旋转角度）
+            Instantiate(bullet, new Vector3(transform.position.x, bullet.transform.position.y, bullet.transform.position.z), bullet.transform.rotation);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //rb.AddForce(0, jumpForce, 0,ForceMode.Impulse); 效果同下行
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
